872fe2a16356625ad00f3a33800de75a46312722
muradpm <abdulkadyr0v@yandex.ru>
2024-06-01 00:05:19 +0300

    update import links to components

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
+import { Footer } from "./_components/footer";
+import { Category } from "./_components/category";
+import { Goods } from "./_components/goods";
 
-export default function Home() {
+const MarketingPage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello!
-    </main>
+    <div className="min-h-full flex flex-col dark:bg-[#1F1F1F]">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Category />
+        <Goods />
+      </div>
+      <Footer />
+    </div>
   );
-}
+};
+
+export default MarketingPage;
