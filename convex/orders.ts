bb7dd9086060b56f4d9a0c0d83a7566b3607e5fe
muradpm <abdulkadyr0v@yandex.ru>
2024-06-12 18:18:20 +0300

    filter goods in cart by userid

--- convex/orders.ts
+++ convex/orders.ts
@@ -2,15 +2,28 @@ import { v } from "convex/values";
 
 import { mutation, query } from "./_generated/server";
 
-export const get = query({
+import { Id } from "./_generated/dataModel";
+
+export const getAll = query({
   handler: async (ctx) => {
-    const orders = await ctx.db.query("orders").collect();
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      return [];
+    }
+
+    const userId = identity.subject as Id<"users">;
+
+    const orders = await ctx.db
+      .query("orders")
+      .filter((q) => q.eq(q.field("userId"), userId))
+      .collect();
 
     return orders;
   },
 });
 
-export const remove = mutation({
+export const removeFromCart = mutation({
   args: { id: v.id("orders") },
   handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
